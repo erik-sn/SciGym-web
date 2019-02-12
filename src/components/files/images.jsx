b4605f1b778cac46b4fede4ad9d1499705d17d28
Unknown <hendrik.poulsen-nautrup@uibk.ac.at>
2019-02-12 23:38:32 +0100

    First version of a Get Started
    + adjusted Hero image a little
    + adjusted the loading symbol to be placed in the middle

--- src/components/files/images.jsx
+++ src/components/files/images.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 // TODO: make svg icons and use SvgIcon as in https://v0.material-ui.com/#/components/svg-icon
-export const Banner = () => <img src='http://localhost:8000/static/images/ai_photonics_banner.jpg' height='60' width='468' alt=""/>;
+export const Banner = () => <img src='http://localhost:8000/static/images/ai_photonics_banner.jpg' height='60' width='468' alt=""/>
 export const SciGymLogo = () => <img src='http://localhost:8000/static/icons/scigym-logo.png' height="150" width="150" alt=""/>
-export const GithubIcon = () => <img src='http://localhost:8000/static/icons/github-circle.png' height="48" width="48" alt=""/>;
+export const GithubIcon = () => <img src='http://localhost:8000/static/icons/github-circle.png' height="48" width="48" alt=""/>
 export const SciGymIcon = () => <img src='http://localhost:8000/static/icons/scigym-logo.png' height="37" width="37" alt="" style={{padding: "5px"}}/>
+export const RLParadigm = () => <img src='http://localhost:8000/static/images/RLParadigm.gif' height='360' width='480' alt=""/>
